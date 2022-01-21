d6cff2f1e7837a3c67ca9f3b5d1511e62bad955e
karthikadapa <96619487+karthikadapa@users.noreply.github.com>
2022-01-21 17:00:59 +0530

    Update models.py

--- APIapp1/models.py
+++ APIapp1/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 # Create your models here.
 class KarthikModel(models.Model):
-    name = models.CharField(max_length=10)
-    Age = models.IntegerField()    
+    name = models.CharField(max_length=10)  
     id1 = models.IntegerField()
     salary = models.IntegerField()
-    Adress = models.TextField()
+   
   
 
     def __str__(self):
