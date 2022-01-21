2059adf418ed3fde275e84673a019a30a16e45dc
karthikadapa <96619487+karthikadapa@users.noreply.github.com>
2022-01-21 16:23:23 +0530

    joining added

--- APIapp1/models.py
+++ APIapp1/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class KarthikModel(models.Model):
     name = models.CharField(max_length=10)
     Age = models.IntegerField()
+    DOJ = models.IntegerField()
     id1 = models.IntegerField()
     salary = models.IntegerField()
     Adress = models.TextField()
+    
 
     def __str__(self):
         return self.name
