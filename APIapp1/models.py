74f236b02f836ff2882a8443cb063b2a6a91c67b
karthikadapa <96619487+karthikadapa@users.noreply.github.com>
2022-01-21 13:48:44 +0530

    Adress was added

--- APIapp1/models.py
+++ APIapp1/models.py
@@ -5,6 +5,7 @@ class KarthikModel(models.Model):
     name = models.CharField(max_length=10)
     id1 = models.IntegerField()
     salary = models.IntegerField()
+    Adress = models.TextField()
 
     def __str__(self):
         return self.name
